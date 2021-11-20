0c19a07169de0e6016e86911b166bd0e7fe772da
Ramprasad <ramprasad@krititech.in>
2021-11-20 12:26:20 +0530

    Issue Fix

--- src/widgets/GridView.php
+++ src/widgets/GridView.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace ramprasadm1986\gentelella\widgets;
+namespace ramprasadm1986\gentelellacrud\widgets;
 
 use yii\helpers\Html;
+use ramprasadm1986\gentelella\widgets\grid\GridView As GentelellaGridView
 
-class GridView extends \ramprasadm1986\gentelella\widgets\grid\GridView
+class GridView extends GentelellaGridView
 {
     public $options = ['class' => 'dataTables_wrapper'];
     public $layout = "{items}<div class='row'><div class='col-sm-5 col-xs-12'>{summary}</div><div class='col-sm-7 col-xs-12'>{pager}</div></div>";
